0936304acf8426f9cb2614992f65afee5bda18cd
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-05 12:02:33 +0200

    Create schema for rooms

--- RoomAvailabilityApp/models.py
+++ RoomAvailabilityApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Room(models.Model):
+
+    room_number = models.PositiveIntegerField(default=1,primary_key=True,verbose_name='Numer pokoju')
+    room_floor = models.PositiveIntegerField(default=0,primary_key=True,
+                                             validators=[MaxValueValidator(2)],verbose_name='Piętro')
+    room_name = models.CharField(max_length=150,blank=True,null=True,verbose_name='Nazwa pokoju')
+    building_name = models.CharField(max_length=1,blank=True,null=True,verbose_name='Nazwa budynku')
+    additional_room_info = models.TextField(max_length=3_000,blank=True,null=True,verbose_name='Dodatkowe informacje')
+
+    creation_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Sala"
+        verbose_name_plural = "Sale"
+
+    def __str__(self):
+        return (f"Numer sali: {self.room_number}\nPiętro: {self.room_floor}"
+                f"{"\n Nazwa sali: {self.room_name}" if self.room_name else ""}")
+
