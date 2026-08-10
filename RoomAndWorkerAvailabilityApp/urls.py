a669776d5f6c4320601da4ca5d0187b6df5b601c
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-10 12:51:02 +0200

    Add URL to absence details

--- RoomAndWorkerAvailabilityApp/urls.py
+++ RoomAndWorkerAvailabilityApp/urls.py
@@ -4,9 +4,10 @@ from django.contrib.auth import views as auth_views
 app_name = "availability"
 urlpatterns = [
     path("register/", views.register, name="register_worker"),
-path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.worker_list, name="workers_list"),
     path("worker/<int:pk>/confirm-status/",views.confirm_worker_status,name="confirm_worker_status",),
-path("worker/<int:pk>/change-status/",views.change_worker_status,name="change_worker_status",),
+    path("worker/<int:pk>/change-status/",views.change_worker_status,name="change_worker_status",),
+    path("absence-details/<int:pk>/",views.show_absence_details,name="absence_details"),
 ]
