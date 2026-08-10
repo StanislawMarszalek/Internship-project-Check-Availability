edd1c674c1ce8db234964274ca244e4ce8b6e1d5
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-10 14:44:17 +0200

    Add URL for rooms

--- RoomAndWorkerAvailabilityApp/urls.py
+++ RoomAndWorkerAvailabilityApp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("worker/<int:pk>/confirm-status/",views.confirm_worker_status,name="confirm_worker_status",),
     path("worker/<int:pk>/change-status/",views.change_worker_status,name="change_worker_status",),
     path("absence-details/<int:pk>/",views.show_absence_details,name="absence_details"),
+    path("rooms/",views.rooms_list,name="rooms_list"),
 ]
