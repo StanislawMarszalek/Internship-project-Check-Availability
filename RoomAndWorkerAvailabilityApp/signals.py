8f1876cf9ed5d64a50eec82ec87dfa2b5cfb77db
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-12 08:14:25 +0200

    Add 'signals.py' file to  create/delete claendars

---
+++ RoomAndWorkerAvailabilityApp/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from schedule.models import Calendar
+
+from .models import Room
+
+
+@receiver(post_save, sender=Room)
+def create_room_calendar(sender, instance, created, **kwargs):
+    if created:
+        Calendar.objects.get_or_create_calendar_for_object(
+            instance,
+            name=f"Room {instance.id}",
+        )
+
+
+@receiver(post_delete, sender=Room)
+def delete_room_calendar(sender, instance, **kwargs):
+    try:
+        calendar = Calendar.objects.get_calendar_for_object(instance)
+    except Calendar.DoesNotExist:
+        return
+
+    calendar.delete()
