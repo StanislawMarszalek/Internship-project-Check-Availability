1804713dc589684baecf3d5e915f75eb99ebb9d4
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-12 14:21:26 +0200

    Add filtering in rule view

--- RoomAndWorkerAvailabilityApp/views.py
+++ RoomAndWorkerAvailabilityApp/views.py
@@ -274,13 +274,28 @@ class MyFullCalendarView(LoginRequiredMixin, TemplateView):
 def rules_list(request):
     if not request.user.is_superuser:
         return HttpResponseForbidden(
-            "You are not the user or the admin"
+            "You are the admin"
         )
     rules = Rule.objects.all()
+
+    name_query=request.GET.get("name", "").strip()
+    description_query=request.GET.get("description", "").strip()
+    frequency_query=request.GET.get("frequency", "").strip()
+
+    if name_query:
+        rules = rules.filter(name__icontains=name_query)
+    if description_query:
+        rules = rules.filter(description__icontains=description_query)
+    if frequency_query:
+        rules = rules.filter(frequency__icontains=frequency_query)
+
     return render(
         request,
         template_name="showing_data/calendar_rules.html",
         context={
             "rules":rules,
+            "name_query":name_query,
+            "description_query":description_query,
+            "frequency_query":frequency_query,
         }
     )
