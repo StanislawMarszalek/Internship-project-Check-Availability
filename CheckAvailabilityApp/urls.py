ce418b0b5346c9b18360ae4d6ae55f57551c2142
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-13 09:29:31 +0200

    Add URL for reseting password

--- CheckAvailabilityApp/urls.py
+++ CheckAvailabilityApp/urls.py
@@ -35,12 +35,27 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from django.contrib import admin
 from django.urls import include, path
 from RoomAndWorkerAvailabilityApp import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("RoomAndWorkerAvailabilityApp.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
+        ), name="password_reset"),
+
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="registration/password_reset_done.html"
+    ), name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="registration/password_reset_confirm.html"
+    ), name="password_reset_confirm"),
+
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="registration/password_reset_complete.html"
+    ), name="password_reset_complete"),
     path(
         "schedule/event/create/<slug:calendar_slug>/",
         views.CustomCreateEventView.as_view(),
