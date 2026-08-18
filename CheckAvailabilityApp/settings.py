e2e38177a2be6f493089a7c80c200208ad063fe6
Stanislaw Marszalek <stanislawmarszalek890@gmail.com>
2026-08-18 11:17:05 +0200

    Add reading scret_key from .env file

--- CheckAvailabilityApp/settings.py
+++ CheckAvailabilityApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#gb9b^7tgsfnnigr^q713we=_rgspc6$#fubzxnt(+8gmnw$z&'
+SECRET_KEY = os.environ.get('secret_key_check_availability')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
